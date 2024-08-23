93b02592128f7361352f4f0e03d71866379089c4
Willow <98659333+Willow-Development@users.noreply.github.com>
2024-08-22 23:22:33 -0400

    Create sprint.lua

---
+++ sprint.lua
@@ -0,0 +1,31 @@
+local UserInputService = game:GetService("UserInputService")
+local Players = game:GetService("Players")
+
+local player = Players.LocalPlayer
+local character = player.Character or player.CharacterAdded:Wait()
+local humanoid = character:WaitForChild("Humanoid")
+
+local walkSpeed = 16
+local sprintSpeed = 30
+
+-- Function to update walk speed
+local function updateWalkSpeed(isSprinting)
+	if isSprinting then
+		humanoid.WalkSpeed = sprintSpeed
+	else
+		humanoid.WalkSpeed = walkSpeed
+	end
+end
+
+-- Sprint
+UserInputService.InputBegan:Connect(function(input)
+	if input.KeyCode == Enum.KeyCode.LeftShift then
+		updateWalkSpeed(true)
+	end
+end)
+
+UserInputService.InputEnded:Connect(function(input)
+	if input.KeyCode == Enum.KeyCode.LeftShift then
+		updateWalkSpeed(false)
+	end
+end)
